da7cdb3920fe906ae02a83169e17c55501a606c7
Neil Kodner <nkodner@gmail.com>
2011-06-17 10:33:32 -0400

    cleaned up, added some basic analysis

--- oow.py
+++ oow.py
@@ -16,9 +16,7 @@ votes = defaultdict(int)
 
 VOTES_LIMIT = 5
 
-# create gephi file, write the header
-out_file = open('oow.gdf', 'w')
-out_file.write('nodedef> name VARCHAR, label VARCHAR, type VARCHAR\n')
+
 
 # sessions first
 # for the time being, we don't care about the session itself, just who 
@@ -28,11 +26,16 @@ for line in f:
 	session_id,session_name, session_created_by = line.strip().split('|')
 	session_id = int(session_id)
 	session_created_by = int(session_created_by)
+	
 	# session name gets imported in double-quotes, lets get rid of the quotes
 	session_name = session_name.split('"')[1]
+	
 	sessions[session_id] = {'name': session_name, 'created_by': session_created_by}
+	
 	if session_created_by not in person.values():
-		person[(session_created_by)] = {"name": "session_creator-%s" % session_id, "type" : "session creator"}
+		person[(session_created_by)] = {"name": "session_creator-%s" % session_id,
+		 	"type" : "session creator"}
+		
 f.close()
 
 # read the voters, write to a file
@@ -44,12 +47,17 @@ for line in f:
 	person[voter_id] = {"name": voter_name, "type": "voter"}
 f.close()
 
-# now that our nodes are complete, write them to a file
+# now that our nodes are complete, write them to the gdf file
+# create gephi file, write the header
+out_file = open('oow.gdf', 'w')
+out_file.write('nodedef> name VARCHAR, label VARCHAR, type VARCHAR\n')
+
+# write the nodes. leave the file open because we need to write edges
 for id, vals in person.iteritems():
 	out = '%s,%s,%s\n' % (id, vals['name'],vals['type'])
 	out_file.write(out)
 
-# read the votes
+# read the votes, store in a dict where the key is (voter, session_created_by)
 f = open('data/votes.dat')
 for line in f:
 	session_id, voter_id = line.strip().split('|')
@@ -58,14 +66,11 @@ for line in f:
 	votes[(voter_id, sessions[session_id]['created_by'])] += 1
 f.close()
 
-# add nodes for people who have created a session but have not voted
-
 # in this case, the source is the voter and the target is the session creator
 out_file.write('edgedef> source VARCHAR, target VARCHAR, vote_cnt INT\n')
 for (v1,v2),vote_cnt in votes.iteritems():
 	out = "%s,%s,%s\n" %  (v1,v2,vote_cnt)
 	out_file.write(out)
-
 
 out_file.close()
 
@@ -73,8 +78,9 @@ out_file.close()
 # lets see who has voted for who the most times
 srtd_by_votes = sorted(votes.iteritems(),key=operator.itemgetter(1))
 
-# lets identify anyone who voted for someone else at least 5 times
+# lets identify anyone who voted for someone else at least VOTES_LIMIT times
 top_combos = [x for x in srtd_by_votes if x[1] >= VOTES_LIMIT]
+print "people who have voted for someone at least %d times" % VOTES_LIMIT
 for itm in top_combos:
 	print "%s voted for %s %d times" % (person[itm[0][0]]['name'], person[itm[0][1]]['name'], itm[1])
 
