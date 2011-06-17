6346410f2450c83a4b6e93385831d1e11d0728be
Neil Kodner <nkodner@gmail.com>
2011-06-17 10:30:55 -0400

    cleaned up, added some basic analysis

--- oow.py
+++ oow.py
@@ -1,72 +1,80 @@
 #!/usr/bin/python
 from collections import defaultdict
+import operator
 
-	
-# create gephi file
-out_file = open('oow.gdf', 'w')
-out_file.write('nodedef> name VARCHAR, label VARCHAR, type VARCHAR\n')
+
+# note: the reason I'm multiplying session_id * 2 is because the source data
+# has overlap between voter_id and session_id. Hopefully there is no
+# overlap between voter_id and session_creator_id. I have not yet
+# tested for that. Gephi will warn me, on the import, if we re-use
+# an id.
+
 person = {}
 sessions = {}
 votes = defaultdict(int)
 
 
+VOTES_LIMIT = 5
+
+# create gephi file, write the header
+out_file = open('oow.gdf', 'w')
+out_file.write('nodedef> name VARCHAR, label VARCHAR, type VARCHAR\n')
+
+# sessions first
+# for the time being, we don't care about the session itself, just who 
+# created it
+f = open('data/sessions.dat')
+for line in f:
+	session_id,session_name, session_created_by = line.strip().split('|')
+	session_id = int(session_id)
+	session_created_by = int(session_created_by)
+	# session name gets imported in double-quotes, lets get rid of the quotes
+	session_name = session_name.split('"')[1]
+	sessions[session_id] = {'name': session_name, 'created_by': session_created_by}
+	if session_created_by not in person.values():
+		person[(session_created_by)] = {"name": "session_creator-%s" % session_id, "type" : "session creator"}
+f.close()
+
 # read the voters, write to a file
 f = open('data/voters.dat')
 for line in f:
 	voter_id,voter_name = line.strip().split('|')
+	voter_id = int(voter_id)
 	voter_name = voter_name.replace('"','')
-	out = "%s,%s,voter\n" % (voter_id, voter_name)
+	person[voter_id] = {"name": voter_name, "type": "voter"}
+f.close()
+
+# now that our nodes are complete, write them to a file
+for id, vals in person.iteritems():
+	out = '%s,%s,%s\n' % (id, vals['name'],vals['type'])
 	out_file.write(out)
-	person[voter_id] = voter_name
-f.close()
-# now do the same for sessions.
-# this will be refactored ;)
-
-# read the sessions, write to a file
-# note: we're not yet interested in the actual sessions, just 
-# the relationship between the voters and the session creators
-# (I think...it's still early and i'm supposed to be doing something else ;) )
-f = open('data/sessions.dat')
-for line in f:
-	session_id,session_name, session_created_by = line.strip().split('|')
-	session_id = session_id * 2
-	session_name = session_name.replace('"','')
-	out = "%s,%s,session,%s\n" % (session_id, session_name, session_created_by)
-#	out_file.write(out) 
-	sessions[session_id] = {'name': session_name, 'created_by': session_created_by}
-	# if the creator isn't also a voter
-	# create a node for them anyway
-	if session_created_by not in person.keys():
-		# add the session creator to persons and the gdf file
-		# note, i should made this pass first, then go through the voters
-		# because not every session creator is a voter.
-		person[session_created_by] = "session_creator-%s" % session_id
-		out = "%s,session_creator-%s,session creator\n" % (session_created_by, session_id)
-		out_file.write(out)
-
-f.close()
 
 # read the votes
 f = open('data/votes.dat')
 for line in f:
 	session_id, voter_id = line.strip().split('|')
-	votes[(voter_id, sessions[session_id * 2]['created_by'])] += 1
+	voter_id = int(voter_id)
+	session_id = int(session_id)
+	votes[(voter_id, sessions[session_id]['created_by'])] += 1
 f.close()
 
 # add nodes for people who have created a session but have not voted
 
-
+# in this case, the source is the voter and the target is the session creator
 out_file.write('edgedef> source VARCHAR, target VARCHAR, vote_cnt INT\n')
 for (v1,v2),vote_cnt in votes.iteritems():
 	out = "%s,%s,%s\n" %  (v1,v2,vote_cnt)
 	out_file.write(out)
 
 
-# 
-# for (p1,p2),hit_count in hits.iteritems():
-# 
-# 	f.write('%d,%d,%d\n' % (p1,p2, hit_count ))
-# f.close()
-
-
 out_file.close()
+
+# now the fun part.
+# lets see who has voted for who the most times
+srtd_by_votes = sorted(votes.iteritems(),key=operator.itemgetter(1))
+
+# lets identify anyone who voted for someone else at least 5 times
+top_combos = [x for x in srtd_by_votes if x[1] >= VOTES_LIMIT]
+for itm in top_combos:
+	print "%s voted for %s %d times" % (person[itm[0][0]]['name'], person[itm[0][1]]['name'], itm[1])
+
