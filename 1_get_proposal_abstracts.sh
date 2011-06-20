9a896e207570e915d655ac5951dfe1bc2cc2b94d
Greg Rahn <greg@structureddata.org>
2011-06-20 10:38:06 -0700

    final data set modifications

--- 1_get_proposal_abstracts.sh
+++ 1_get_proposal_abstracts.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ## as of June 17th there are 39 pages of proposals sorted by title
+## as of June 19th there are 41 pages of proposals sorted by title
 ## see https://mix.oracle.com/events/oow11/proposals to determine this value
 
 cd proposal_abstracts
 rm *.html
-for i in {1..39}
+for i in {1..41}
 do
 curl -o $i.html "https://mix.oracle.com/events/oow11/proposals?campaign_id=oow11&page=${i}&sort=title"
 done
