2e53f63e5a57e079e9d2a0dd45f10d618fb21019
Neil Kodner <nkodner@gmail.com>
2011-06-20 21:41:50 -0400

    test

--- load_data.sql
+++ load_data.sql
@@ -22,12 +22,24 @@ user_id int primary key,
 name varchar(100)
 );
 
+<<<<<<< Updated upstream
+=======
+LOAD DATA LOCAL INFILE 'data/sessions.dat' INTO TABLE sessions FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
+LOAD DATA LOCAL INFILE 'data/votes.dat'    INTO TABLE votes    FIELDS TERMINATED BY '|' (session_id, user_id);
+LOAD DATA LOCAL INFILE 'data/users.dat'    INTO TABLE users    FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
+
+
+>>>>>>> Stashed changes
 create table final_vote_count (
 session_id int,
 total_votes int
 );
 
+<<<<<<< Updated upstream
 LOAD DATA LOCAL INFILE 'sessions.dat' INTO TABLE sessions FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
 LOAD DATA LOCAL INFILE 'votes.dat'    INTO TABLE votes    FIELDS TERMINATED BY '|' (session_id, user_id);
 LOAD DATA LOCAL INFILE 'users.dat'    INTO TABLE users    FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
 LOAD DATA LOCAL INFILE 'final_vote_count.dat' INTO TABLE final_vote_count FIELDS TERMINATED BY '|';
+=======
+LOAD DATA LOCAL INFILE 'data/final_vote_count.dat' INTO TABLE final_vote_count FIELDS TERMINATED BY '|';
+>>>>>>> Stashed changes
