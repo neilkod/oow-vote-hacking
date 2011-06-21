46b1000c39f6c25de79538a742d6a891f4937f55
Greg Rahn <greg@structureddata.org>
2011-06-20 20:57:13 -0700

    added final_vote_count

--- load_data.sql
+++ load_data.sql
@@ -22,31 +22,12 @@ user_id int primary key,
 name varchar(100)
 );
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-LOAD DATA LOCAL INFILE 'data/sessions.dat' INTO TABLE sessions FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
-LOAD DATA LOCAL INFILE 'data/votes.dat'    INTO TABLE votes    FIELDS TERMINATED BY '|' (session_id, user_id);
-LOAD DATA LOCAL INFILE 'data/users.dat'    INTO TABLE users    FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
-
-
->>>>>>> Stashed changes
 create table final_vote_count (
-session_id int,
+session_id int primary key,
 total_votes int
 );
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 LOAD DATA LOCAL INFILE 'sessions.dat' INTO TABLE sessions FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
 LOAD DATA LOCAL INFILE 'votes.dat'    INTO TABLE votes    FIELDS TERMINATED BY '|' (session_id, user_id);
 LOAD DATA LOCAL INFILE 'users.dat'    INTO TABLE users    FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
 LOAD DATA LOCAL INFILE 'final_vote_count.dat' INTO TABLE final_vote_count FIELDS TERMINATED BY '|';
-=======
-LOAD DATA LOCAL INFILE 'data/final_vote_count.dat' INTO TABLE final_vote_count FIELDS TERMINATED BY '|';
->>>>>>> Stashed changes
-=======
-LOAD DATA LOCAL INFILE 'data/final_vote_count.dat' INTO TABLE final_vote_count FIELDS TERMINATED BY '|';
->>>>>>> Stashed changes
