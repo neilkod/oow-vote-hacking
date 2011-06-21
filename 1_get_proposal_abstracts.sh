d1fbd3645a2d872b81c91283b21715bb68ea2e24
Greg Rahn <greg@structureddata.org>
2011-06-20 18:28:51 -0700

    final data set of all 43 abstracts

--- 1_get_proposal_abstracts.sh
+++ 1_get_proposal_abstracts.sh
@@ -2,11 +2,12 @@
 
 ## as of June 17th there are 39 pages of proposals sorted by title
 ## as of June 19th there are 41 pages of proposals sorted by title
+## final count are 43 pages of proposals sorted by title
 ## see https://mix.oracle.com/events/oow11/proposals to determine this value
 
 cd proposal_abstracts
 rm *.html
-for i in {1..41}
+for i in {1..43}
 do
 curl -o $i.html "https://mix.oracle.com/events/oow11/proposals?campaign_id=oow11&page=${i}&sort=title"
 done
