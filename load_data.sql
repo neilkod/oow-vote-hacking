781b5f717fbba661450189c09a281e8db05d8a34
Greg Rahn <greg@structureddata.org>
2011-06-21 17:44:05 -0700

    latest updates

--- load_data.sql
+++ load_data.sql
@@ -4,6 +4,7 @@ drop table sessions;
 drop table votes;
 drop table users;
 drop table final_vote_count;
+drop table all_votes;
 
 create table sessions (
 session_id int primary key,
@@ -23,11 +24,18 @@ name varchar(100)
 );
 
 create table final_vote_count (
-session_id int primary key,
+session_id int,
 total_votes int
 );
 
-LOAD DATA LOCAL INFILE 'sessions.dat' INTO TABLE sessions FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
-LOAD DATA LOCAL INFILE 'votes.dat'    INTO TABLE votes    FIELDS TERMINATED BY '|' (session_id, user_id);
-LOAD DATA LOCAL INFILE 'users.dat'    INTO TABLE users    FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
+create table all_votes (
+id integer primary key auto_increment,
+session_id int,
+user_id int
+);
+
+LOAD DATA LOCAL INFILE 'sessions.dat'         INTO TABLE sessions         FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
+LOAD DATA LOCAL INFILE 'votes.dat'            INTO TABLE votes            FIELDS TERMINATED BY '|' (session_id, user_id);
+LOAD DATA LOCAL INFILE 'users.dat'            INTO TABLE users            FIELDS TERMINATED BY '|' OPTIONALLY ENCLOSED BY '"';
 LOAD DATA LOCAL INFILE 'final_vote_count.dat' INTO TABLE final_vote_count FIELDS TERMINATED BY '|';
+LOAD DATA LOCAL INFILE 'all_votes.dat'        INTO TABLE all_votes        FIELDS TERMINATED BY '|' (session_id, user_id);
